8f583fe60f0d6e174a9a68d5d6a71aec3df8da03
congpx-udu <congpx.ptit@gmail.com>
2025-05-09 17:14:53 +0700

    first commit

---
+++ WEB_HLS_CODE/2025-5-9/ThucHanhTrenLop/assets/javascript/main.js
@@ -0,0 +1,24 @@
+alert("hello js");
+confirm("are you ok");
+console.log("udu");
+prompt("bạn bao nhiêu tuổi");
+
+var fullName = "Phạm Xuân Công";
+var age = 19;
+
+if (age > 20) {
+  console.log(`${fullName}" đã đủ tuổi lấy vợ"`);
+} else {
+  console.log(`${fullName}" chưa đủ tuổi lấy vợ"`);
+}
+
+var number = 20;
+if (number === 10) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+for (var i = 0; i < 4; i++) {
+  console.log(`học ptit ${i} năm`);
+}
